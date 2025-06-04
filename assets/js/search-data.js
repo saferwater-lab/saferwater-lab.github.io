c5b1739da03dc6bc82b5a6e0c19549a6f79649b1
ekumpel <ekumpel@users.noreply.github.com>
2025-06-04 21:41:20 +0000

    Deploying to gh-pages from @ saferwater-lab/saferwater-lab.github.io@6513fb4f4c406dc24e0103c46256d9c4ddac0f5d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -456,13 +456,13 @@ ninja.data = [{
           description: "with background image",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
+            },},{id: "projects-lead-in-water-in-schools-and-childcares",
+          title: 'Lead in Water in Schools and Childcares',
+          description: "Research and technical assistance to measure and remediate lead in water in schools and childcares",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
+            },},{id: "projects-test",
+          title: 'test',
           description: "a project that redirects to another website",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
